870a2af5e916f0a2c66d8355184999f473087533
Bohdan <bohdan.chornyi@white-test.com>
2024-11-16 03:23:15 +0200

    parallel run

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,15 +15,13 @@ export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
-  metadata: {
-    tags: process.env.PARAM_TAG || 'bohdan-wtl'
-  },
+
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   //retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 20 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter:[
     ['html', { outputFolder: 'playwright-report', open: 'never' }]
